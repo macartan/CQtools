4561dcf3a9c44e5a83b92fab0bd2e249932371ba
macartan <macartan@gmail.com>
2020-06-21 22:42:40 +0200

    fix loo

--- R/log_likelihoods.R
+++ R/log_likelihoods.R
@@ -5,12 +5,13 @@
 #' @inheritParams CQtools_internal_inherit_params
 #' @param data Data in long format
 #' @param sims Number of simulations
+#' @param ... arguments passed to update_model
 #' @export
 #' @examples
 #' model <- make_model("X->Y")
 #' data <- simulate_data(model, n = 4)
 #' get_loo_likelihood(data, model)
-get_loo_likelihood <- function(data, model, sims=100){
+get_loo_likelihood <- function(data, model, sims=100, ...){
     
     short_data <- collapse_data(data, model)
     rows       <- 1:nrow(short_data)
@@ -18,11 +19,12 @@ get_loo_likelihood <- function(data, model, sims=100){
     case_likelihood <- lapply(rows, 
                               function(j) {
                                   loo_data <-  mutate(short_data, count = ifelse(count>0 & rows == j, count-1, count))
-                                  loo_left <-  mutate(short_data, count = ifelse(count>0 & rows == j, 1, 0))
-                                  loo_post <-  update_model(model, loo_data, data_type = "compact")
+                                  loo_left <-  mutate(short_data, count = ifelse(count>0 & rows == j, 1, 0)) %>%
+                                      expand_data(model)
+                                  loo_post <-  update_model(model, loo_data, data_type = "compact", ...)
                                   k <- min(sims, nrow(loo_post$posterior_distribution))
                                   loo_prob <-  apply(loo_post$posterior_distribution[1:k, ], 1, function(pars)
-                                      get_data_probs(model, loo_left, parameters = pars)[j])
+                                      get_data_probs(model, loo_left, parameters = pars)[short_data$event[j]])
                                   mean(loo_prob)}
     )
     
